c8c42262f1d735239875d56dc75a25f743abd0e1
sarthak badola <sarthakbadola97@gmail.com>
2023-11-30 21:22:21 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
+import Chai from './chai';
 
 function App() {
+  let [counter,setCounter]= useState(15)
+ // let counter = 15;
+const addvalue = () =>{
+  console.log("clicked",counter)
+  counter = counter + 1;
+  setCounter(counter)
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   <Chai/>
+     <h1>time for some action of hooks {counter}</h1>
+       
+      <button  onClick={addvalue}>
+        Addme {counter}</button>
+        <br></br>
+        <footer> hello {counter}</footer>
+   </>
+
   );
 }
 
